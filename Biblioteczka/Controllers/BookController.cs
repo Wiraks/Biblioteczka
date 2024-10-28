b2d4f1cea49695147f41d12846fd54c30bf96d92
wiraks <g.kluczynski@wp.pl>
2024-10-28 14:51:42 +0100

    First classes

---
+++ Biblioteczka/Controllers/BookController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Biblioteczka.Models;
+
+namespace Biblioteczka.Controllers
+{
+    public class BookController : Controller
+    {
+
+        [NonAction]
+        public List<Book> GetBookList()
+        {
+            return new List<Book>{
+                  new Book{
+                     ID = 1,
+                     Name = "Gra o tron",
+                     Author = "Martin George R. R.",
+                     PublishingHouse = "Wydawnictwo Zysk i S-ka",
+                     ReleaseDate = 2011,
+                     NumberOfPages= 844,
+                     BookBinding = "Miękka",
+                     ISBN = "978-83-7506-729-3",
+                     OwnerID = 1,
+                  },
+                  new Book{
+                     ID = 2,
+                     Name = "Starcie królów",
+                     Author = "Martin George R. R.",
+                     PublishingHouse = "Wydawnictwo Zysk i S-ka",
+                     ReleaseDate = 2012,
+                     NumberOfPages= 1024,
+                     BookBinding = "Twarda",
+                     ISBN = "978-83-8335-275-6",
+                     OwnerID = 1,
+                  },
+            };
+        }
+        // GET: Book
+        public ActionResult Index()
+        {
+            var books = from e in GetBookList()
+                           orderby e.ID
+                           select e;
+            return View(books);
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Book/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ Biblioteczka/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Biblioteczka.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string PublishingHouse { get; set; }
+        public int ReleaseDate { get; set; }
+        public int NumberOfPages { get; set; }
+        public string BookBinding { get; set; }
+        public string ISBN { get; set; }
+        public int OwnerID { get; set; }
+    }
+}
